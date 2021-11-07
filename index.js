5d997e8f0ad8e76a86c332234ac07648e0ecd6db
Sheikhasib <sheikhhasib037@gmail.com>
2021-11-07 20:52:47 +0600

    upsert added

--- index.js
+++ index.js
@@ -19,7 +19,52 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         await client.connect();
-        console.log('database connected successfully');
+        // console.log('database connected successfully');
+        const database = client.db('doctors_portal');
+        const appointmentsCollection = database.collection('appointments');
+        const usersCollection = database.collection('users');
+
+        // From Appointments to get all appointments
+        app.get('/appointments', async(req, res) => {
+          const email = req.query.email;
+          const date = new Date(req.query.date).toLocaleDateString();
+          // console.log(date);
+          const query = {email: email, date: date}
+          // console.log(query);
+          const cursor = appointmentsCollection.find(query);
+          const appointments = await cursor.toArray();
+          res.json(appointments);
+        })
+
+        // From BookingModal to post single appointment 
+        app.post('/appointments', async(req, res) => {
+          const appointment = req.body;
+          // console.log(appointment);
+          const result = await appointmentsCollection.insertOne(appointment);
+          // console.log(result);
+          res.json(result)
+        })
+
+        // From useFirebase to post user data
+        app.post('/users', async(req, res) => {
+          const user = req.body;
+          const result = await usersCollection.insertOne(user);
+          console.log(result);
+          res.json(result);
+        })
+
+        // From useFirebase to Updating user data
+        app.put('/users', async(req, res) => {
+          const user = req.body;
+          console.log('put', user);
+          const filter = {email: user.email};
+          const options = { upsert: true };
+          const updateDoc = {
+            $set: user
+          };
+          const result = await usersCollection.updateOne(filter, updateDoc, options);
+          res.json(result);
+        })
     }
     finally{
         // await client.close();
@@ -35,3 +80,11 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+// app.get('/users')
+// app.post('/users')
+// app.get('/users/:id')
+// app.put('/users/:')
+// app.delete('/users/:')
+// users:get
+// users:post
