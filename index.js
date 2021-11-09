3a91070976db0beed4f7aae8e698a1f2f744d7c2
Sheikhasib <sheikhhasib037@gmail.com>
2021-11-09 21:34:58 +0600

    added admin service

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient } = require('mongodb');
+const cors = require("cors");
+const admin = require("firebase-admin");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
 
 const port = process.env.PORT || 5000;
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+// doctors-portal-firebase-adminsdk.json
+// const serviceAccount = require("./doctors-portal-firebase-adminsdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 // middleware
 app.use(cors());
@@ -14,72 +24,122 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // console.log(uri);
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-async function run(){
-    try{
-        await client.connect();
-        // console.log('database connected successfully');
-        const database = client.db('doctors_portal');
-        const appointmentsCollection = database.collection('appointments');
-        const usersCollection = database.collection('users');
+async function verifyToken(req, res, next) {
+  if (req.headers?.authorization?.startsWith("Bearer ")) {
+    const token = req.headers.authorization.split(" ")[1];
 
-        // From Appointments to get all appointments
-        app.get('/appointments', async(req, res) => {
-          const email = req.query.email;
-          const date = new Date(req.query.date).toLocaleDateString();
-          // console.log(date);
-          const query = {email: email, date: date}
-          // console.log(query);
-          const cursor = appointmentsCollection.find(query);
-          const appointments = await cursor.toArray();
-          res.json(appointments);
-        })
+    try {
+      const decodedUser = await admin.auth().verifyIdToken(token);
+      req.decodedEmail = decodedUser.email;
+    } catch {}
+  }
+  next();
+}
 
-        // From BookingModal to post single appointment 
-        app.post('/appointments', async(req, res) => {
-          const appointment = req.body;
-          // console.log(appointment);
-          const result = await appointmentsCollection.insertOne(appointment);
-          // console.log(result);
-          res.json(result)
-        })
+async function run() {
+  try {
+    await client.connect();
+    // console.log('database connected successfully');
+    const database = client.db("doctors_portal");
+    const appointmentsCollection = database.collection("appointments");
+    const usersCollection = database.collection("users");
 
-        // From useFirebase to post user data
-        app.post('/users', async(req, res) => {
-          const user = req.body;
-          const result = await usersCollection.insertOne(user);
-          console.log(result);
-          res.json(result);
-        })
+    // From Appointments to get all appointments
+    app.get("/appointments", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const date = new Date(req.query.date).toLocaleDateString();
+      // console.log(date);
+      const query = { email: email, date: date };
+      // console.log(query);
+      const cursor = appointmentsCollection.find(query);
+      const appointments = await cursor.toArray();
+      res.json(appointments);
+    });
 
-        // From useFirebase to Updating user data
-        app.put('/users', async(req, res) => {
-          const user = req.body;
-          console.log('put', user);
-          const filter = {email: user.email};
-          const options = { upsert: true };
-          const updateDoc = {
-            $set: user
-          };
-          const result = await usersCollection.updateOne(filter, updateDoc, options);
-          res.json(result);
-        })
-    }
-    finally{
-        // await client.close();
-    }
+    // From BookingModal to post single appointment
+    app.post("/appointments", async (req, res) => {
+      const appointment = req.body;
+      // console.log(appointment);
+      const result = await appointmentsCollection.insertOne(appointment);
+      // console.log(result);
+      res.json(result);
+    });
+
+    // From useFirebase to get special user information
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+
+    // From useFirebase to post user data
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      console.log(result);
+      res.json(result);
+    });
+
+    // From useFirebase to Updating user data
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      // console.log('put', user);
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
+    // From MakeAdmin to update user admin
+    app.put('/users/admin', verifyToken, async (req, res) => {
+      const user = req.body;
+      const requester = req.decodedEmail;
+      if (requester) {
+          const requesterAccount = await usersCollection.findOne({ email: requester });
+          if (requesterAccount.role === 'admin') {
+              const filter = { email: user.email };
+              const updateDoc = { $set: { role: 'admin' } };
+              const result = await usersCollection.updateOne(filter, updateDoc);
+              res.json(result);
+          }
+      }
+      else {
+          res.status(403).json({ message: 'you do not have access to make admin' })
+      }
+
+  })
+  } 
+  finally {
+    // await client.close();
+  }
 }
 
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello Doctors Portal!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello Doctors Portal!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
 // app.get('/users')
 // app.post('/users')
@@ -88,3 +148,7 @@ app.listen(port, () => {
 // app.delete('/users/:')
 // users:get
 // users:post
+
+
+// DB_USER=doctorDB
+// DB_PASS=X4OB0c22mAXdBybp
